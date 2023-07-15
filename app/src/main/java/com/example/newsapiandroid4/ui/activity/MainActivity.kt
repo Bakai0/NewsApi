6f3efd8f46114dcd00f6be1ec39362e8193dd557
Bakai0 <ulanbekovbakai00@gmail.com>
2023-07-15 11:20:51 +0300

    newsApi

--- app/src/main/java/com/example/newsapiandroid4/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/newsapiandroid4/ui/activity/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.newsapiandroid4.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.newsapiandroid4.R
+import com.example.newsapiandroid4.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupNav()
+    }
+
+    private fun setupNav() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNav.setupWithNavController(navController)
     }
 }
